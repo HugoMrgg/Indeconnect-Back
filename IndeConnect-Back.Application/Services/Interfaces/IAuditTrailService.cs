bbcae9a49f9a4f80e08b831bd6e86969fb7b6979
Hugo Murgia <hg.murgia@gmail.com>
2025-11-18 15:41:38 +0100

    start of the historic of the interactions with bd

---
+++ IndeConnect-Back.Application/Services/Interfaces/IAuditTrailService.cs
@@ -0,0 +1,6 @@
+﻿namespace IndeConnect_Back.Application.Services.Interfaces;
+
+public interface IAuditTrailService
+{
+    Task LogAsync(string action, long? userId = null, string? details = null);
+}
